4090636665a443080bc678aaa74187c1de65cda5
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-01-16 07:23:54 -0300

    updating

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function funcao_calcular(){
     var n1 = parseFloat(document.getElementById("altura").value)
     var n2 = parseFloat(document.getElementById("peso").value)
-    // var res = Number(document.getElementById("resultado").value)
-    var total = window.document.getElementById('resultado')
-    var novo_n1 = (n1 * n1)
-    soma = n2 / novo_n1
-    total.innerHTML= `<strong>Resultado:</strong>${soma}` 
+    var resultado = window.document.getElementById('resultado')
+    var soma = n2/(n1*n1)
+    if(soma <=18.5){
+    resultado.innerHTML=`RESULTADO:
+    ${soma}<br><br>
+    Você está <strong>Abaixo do Peso </strong>`
+    }else if(soma >=18.5 && soma <=24.9   ){
+        resultado.innerHTML= `RESULTADO:
+        ${soma}<br><br>
+        Você está <strong> no Peso Normal </strong>`
+    }else{
+        alert('[Erro: aplicação em desenvolvimento]')
+
+    }
 }

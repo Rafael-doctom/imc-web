e29a26f239b313cd84c2b0118244b95f87f66820
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2023-01-02 14:56:37 -0300

    update

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-function funcao_calcular() {
 
-    let weight = parseFloat(document.getElementById("altura").value);
-    let height = parseFloat(document.getElementById("peso").value);
-    let result = window.document.getElementById('resultado');
+let calculator = document.getElementById('calculator');
 
-    let soma = height / (weight * weight)
+calculator.addEventListener('click', () => {
 
-    if (soma <= 18.5) {
-        result.innerHTML = `${soma} <br>
-    Você está <strong>Abaixo do Peso </strong>`
-    } else if (soma >= 18.5 && soma <= 24.9) {
-        result.innerHTML = `
-        ${soma}<br>
-        Você está <strong> no Peso Normal </strong>`
-    } else {
-        alert('[Erro: aplicação em desenvolvimento]')
-    }
+    const height = parseFloat(document.getElementById('height').value);
+    const weight = parseFloat(document.getElementById('weight').value);
+    const result = document.getElementById('result');
 
-}
+    let sumToImc = weight / (height * height);
+    let thinness = sumToImc <= 18.5;
+    let normal = sumToImc >= 18.5 && sumToImc <= 24.9;
+    let overweight = sumToImc >= 25 && sumToImc <= 29.9;
+    let obesity = sumToImc >= 30 && sumToImc <= 39.9;
+    let larger = sumToImc >= 40;
+
+    if (thinness) {
+        result.innerText = `Abaixo do Peso: ${sumToImc}`;
+    };
+
+    if (normal) {
+        result.innerText = `Normal: ${sumToImc}`;
+    };
+
+    if (overweight) {
+        result.innerText = `Sobrepeso: ${sumToImc}`;
+    };
+
+    if (obesity) {
+        result.innerText = `Obesidade: ${sumToImc}`;
+    };
+
+    if (larger) {
+        result.innerText = `Obesidade grave: ${sumToImc}`;
+    };
+
+});
